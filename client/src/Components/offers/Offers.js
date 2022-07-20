d8c65b8663cecdb2cc689eebbb4123a1da03dafb
Guillermo García <guillermieres97@gmail.com>
2022-07-21 00:41:05 +0200

    adding styles to page createads

--- client/src/Components/offers/Offers.js
+++ client/src/Components/offers/Offers.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Offers = () => {
   const car = {
-    img: "https://firebasestorage.googleapis.com/v0/b/marketplace-64f7d.appspot.com/o/coches%2Fda89a351c530c87afd2bf9c7798a5e85-8650624-ebdd99a62f0c0c32166891228d5f83f7222588b8-vehicle-gallery-large.jpg?alt=media&token=362227f8-7ad5-4a40-aed6-cc8dda9e58f6",
+    img: {
+        seat: "https://res.cloudinary.com/dag3rmkxg/image/upload/f_auto,c_limit,w_750,q_auto/v1/car_model/seat/arona",
+        renault: "https://firebasestorage.googleapis.com/v0/b/marketplace-64f7d.appspot.com/o/coches%2Fda89a351c530c87afd2bf9c7798a5e85-8650624-ebdd99a62f0c0c32166891228d5f83f7222588b8-vehicle-gallery-large.jpg?alt=media&token=362227f8-7ad5-4a40-aed6-cc8dda9e58f6",
+        nissan: "https://res.cloudinary.com/dag3rmkxg/image/upload/f_auto,c_limit,w_750,q_auto/v1/car_model/nissan/juke"
+  },
+
     brand: "Renault Scenic",
     model: "2016",
     color: "Blanco",
@@ -17,7 +22,7 @@ const Offers = () => {
         <div>
             <div className="content-car">
                 <div>
-                    <img src={car.img} alt="" />
+                    <img src={car.img.seat} alt="" />
                     <div className="content-info">
                         <div>
                         <p className="title-brand">{car.brand}</p>
@@ -35,7 +40,7 @@ const Offers = () => {
                 </div>      
 
                   <div>
-                    <img src={car.img} alt="" />
+                    <img src={car.img.nissan} alt="" />
                     <div className="content-info">
                         <div>
                         <p className="title-brand">{car.brand}</p>
@@ -53,7 +58,7 @@ const Offers = () => {
                 </div>      
 
                   <div>
-                    <img src={car.img} alt="" />
+                    <img src={car.img.renault} alt="" />
                     <div className="content-info">
                         <div>
                         <p className="title-brand">{car.brand}</p>
--- client/src/Components/pages/CreateAds.js
+++ client/src/Components/pages/CreateAds.js
@@ -2,8 +2,59 @@ import React from 'react';
 
 const CreateAds = () => {
     return (
-        <div>
-            Creando Anuncio
+        <div className='App'>
+            <nav>
+                <img src="https://firebasestorage.googleapis.com/v0/b/marketplace-64f7d.appspot.com/o/img%2FlogoMarketPlace.svg?alt=media&token=637841f3-155a-4a11-b430-10c86fd719fb" alt="" />
+                <button type='button'>Inicio</button>
+            </nav>
+            <div className='headerForm'>
+                <h2 className='headerForm-title'>Renting para tu empresa<br></br>No esperes más<br></br>Anúnciate!</h2>
+                <div>
+                    <img src='https://res.cloudinary.com/dag3rmkxg/image/upload/w_1050,,c_pad,q_auto,g_south/v1/car_model/seat/arona' alt='' />
+                </div>
+            </div>
+            <h2 className='beforeTitle'>Datos de tu anuncio</h2>
+            <form>
+                <div className='file'>
+                    <label>Fotografía del anuncio</label>
+                    <input accept="image/jpeg, image/png, image/gif" type="file" />
+                </div>
+                <div className='inputs'>
+                    <label>Marca</label>
+                    <select>
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                        <option value="seat">Seat</option>
+                        <option value="renault">Renault</option>
+                        <option value="ford">Ford</option>
+                        <option value="peugeot">Peugeot</option>
+                        <option value="mercedes">Mercedes</option>
+                        <option value="subaru">Subaru</option>
+                        <option value="citroen">Citroen</option>
+                        <option value="nissan">Nissan</option>
+                        <option value="kia">Kia</option>
+                    </select>
+                    <label>Modelo</label>
+                    <input type="text" />
+                    <label>Color</label>
+                    <select>
+                        <option value="rojo">Rojo</option>
+                        <option value="blanco">Blanco</option>
+                        <option value="negro">Negro</option>
+                        <option value="gris">Gris</option>
+                        <option value="amarillo">Amarillo</option>
+                        <option value="azul">Azul</option>
+                    </select>
+                    <label>Precio</label>
+                    <input type="number" />
+                </div>
+            </form>
+            <div className='buttons'>
+                <button class="backButton" type='button'>Volver</button>
+                <button type='button'>Publicar</button>
+            </div>
         </div>
     );
 };
