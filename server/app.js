9209d84db4ae0ae958446bf2990fe56d9977ffd4
Guillermo García <guillermieres97@gmail.com>
2022-07-20 17:26:39 +0200

    car model

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const cors = require( 'cors' );
 const { categoriesRouter } = require("./routers/categoriesRoutes");
+const { carsRouter } = require("./routers/carsRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/categories', categoriesRouter )
+app.use('/api/v1/cars', carsRouter)
 
 module.exports = {app};
