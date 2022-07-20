5c34bd1ab8cdaa9db5d6fd862270c464d3ffe22d
danielcasta0398 <98791147+danielcasta0398@users.noreply.github.com>
2022-07-20 14:08:19 +0200

    env

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require( 'sequelize' )
+const dotenv = require("dotenv")
+dotenv.config({ path: './config.env' })
 
 const db = new Sequelize({
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'Daniel0119',
-    database: 'marketplace',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
     logging: false
 })
 
